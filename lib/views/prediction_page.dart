2e6133f3f44b4ee52d0d9e1cf60dcdd5164d66ae
DiegoHDLC <dherreradelacalle@gmail.com>
2022-06-04 10:59:56 -0400

    Conversor

--- lib/views/prediction_page.dart
+++ lib/views/prediction_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
 class PredictionPage extends StatefulWidget {
   const PredictionPage({Key? key}) : super(key: key);
@@ -10,6 +13,8 @@ class PredictionPage extends StatefulWidget {
 class _PredictionState extends State<PredictionPage> {
   bool escuchando = false;
   String _text = 'PALABRA TRANSCRITA';
+  final sugerenciaController = TextEditingController();
+  static String newValue = "";
 
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
@@ -46,6 +51,7 @@ class _PredictionState extends State<PredictionPage> {
               padding: EdgeInsets.all(16.0),
               child: TextFormField(
                 textCapitalization: TextCapitalization.words,
+                initialValue: "Dog",
                 decoration: const InputDecoration(
                   border: UnderlineInputBorder(),
                   enabled: true,
@@ -67,9 +73,39 @@ class _PredictionState extends State<PredictionPage> {
                 style: TextStyle(fontSize: 25, fontFamily: 'Dosis'),
               ),
             ),
+            ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                  minimumSize: Size(175, 50),
+                  primary: escuchando ? Colors.red : Color(0xFF476BF6),
+                  onPrimary: Colors.black),
+              onPressed: () {
+                reproducirPalabra();
+              },
+              icon: Icon(escuchando ? Icons.stop : Icons.mic, size: 30),
+              label: Text(
+                escuchando ? 'DETENER' : 'ESCUCHAR',
+                style: TextStyle(fontSize: 25, fontFamily: 'Dosis'),
+              ),
+            ),
           ],
         ),
       ),
     ));
+  }
+
+  Future<void> reproducirPalabra() async {
+    FlutterTts flutterTts = FlutterTts();
+    var result = await flutterTts.speak("Dog");
+    List<dynamic> languages = await flutterTts.getLanguages;
+
+    await flutterTts.setLanguage("en-US");
+
+    await flutterTts.setSpeechRate(0.3);
+
+    await flutterTts.setVolume(1.0);
+
+    await flutterTts.setPitch(0.5);
+
+    await flutterTts.isLanguageAvailable("en-US");
   }
 }
