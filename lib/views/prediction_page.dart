e390d4571ac61377425a809b6040e38b429ae4eb
DiegoHDLC <dherreradelacalle@gmail.com>
2022-06-26 19:08:00 -0400

    el perro corre con una nueva libreria stt

--- lib/views/prediction_page.dart
+++ lib/views/prediction_page.dart
@@ -12,6 +12,8 @@ import 'package:path_provider_platform_interface/path_provider_platform_interfac
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'dart:convert';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 import '../constants.dart';
 
@@ -23,24 +25,24 @@ class PredictionPage extends StatefulWidget {
 }
 
 class _PredictionState extends State<PredictionPage> {
+  @override
+  void initState() {
+    super.initState();
+    _initSpeech();
+  }
+
+  SpeechToText _speechToText = SpeechToText();
   final sugerenciaController = TextEditingController();
   late stt.SpeechToText _speech;
-<<<<<<< HEAD
+  bool _speechEnabled = false;
+  String _lastWords = '';
   String url = "";
   var Data;
   String QueryText = 'Prediccion';
 
   bool escuchando = false;
-=======
   bool _isListening = false;
->>>>>>> 4ade504e25a464a486a2b7cd7736102151087ee7
   String _text = 'Transcripcion';
-
-  @override
-  void initState() {
-    super.initState();
-    _speech = stt.SpeechToText();
-  }
 
   Widget build(BuildContext context) {
     //Python pythonScript = Python();
@@ -71,18 +73,27 @@ class _PredictionState extends State<PredictionPage> {
             TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: QueryText,
-                enabled: false,
+                labelText: _speechToText.isListening
+                    ? '$_lastWords'
+                    // If listening isn't active but could be tell the user
+                    // how to start it, otherwise indicate that speech
+                    // recognition is not yet ready or not supported on
+                    // the target device
+                    : _speechEnabled
+                        ? 'Tap the microphone to start listening...'
+                        : 'Speech not available',
               ),
             ),
-<<<<<<< HEAD
             const SizedBox(height: 10),
             ElevatedButton.icon(
               style: ElevatedButton.styleFrom(
                 minimumSize: Size(175, 50),
                 primary: escuchando ? Colors.red : colorPrimario,
               ),
-              onPressed: _listen,
+              onPressed: _speechToText.isNotListening
+                  ? _startListening
+                  : _stopListening,
+              //onPressed: _listen,
               // onPressed: () async {
               //bool exists = await Directory('lib/holaMundo.py').exists();
               //Directory appDocDir = await getApplicationDocumentsDirectory();
@@ -103,15 +114,6 @@ class _PredictionState extends State<PredictionPage> {
                 style: TextStyle(fontSize: 25, fontFamily: 'Dosis'),
               ),
             ),
-=======
-            SizedBox(height: 10),
-            IconButton(
-              onPressed: _listen, 
-              icon: Icon(_isListening ? Icons.stop_rounded : Icons.play_arrow_rounded),
-              iconSize: 70,
-              color: _isListening ? Color.fromARGB(255, 103, 36, 36) : Color(0xFF243B67) ,
-            )
->>>>>>> 4ade504e25a464a486a2b7cd7736102151087ee7
           ],
         ),
       ),
@@ -131,7 +133,38 @@ class _PredictionState extends State<PredictionPage> {
     await flutterTts.isLanguageAvailable("es-MX");
   }
 
-   void _listen() async {
+  /// Each time to start a speech recognition session
+  void _startListening() async {
+    await _speechToText.listen(onResult: _onSpeechResult);
+    setState(() {});
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _lastWords = result.recognizedWords;
+      url = 'http://127.0.0.1:5000/api?Query=' + _lastWords;
+      Data = GetData(url);
+      //var DecodedData = jsonDecode(Data);
+      //print(DecodedData);
+      //QueryText = DecodedData['Query'];
+    });
+  }
+
+  void _initSpeech() async {
+    _speechEnabled = await _speechToText.initialize();
+    setState(() {});
+  }
+
+  /// Manually stop the active speech recognition session
+  /// Note that there are also timeouts that each platform enforces
+  /// and the SpeechToText plugin supports setting timeouts on the
+  /// listen method.
+  void _stopListening() async {
+    await _speechToText.stop();
+    setState(() {});
+  }
+
+  void _listen() async {
     if (!_isListening) {
       bool available = await _speech.initialize(
         onStatus: (val) => print('onStatus: $val'),
@@ -143,24 +176,21 @@ class _PredictionState extends State<PredictionPage> {
           onResult: (val) => setState(() async {
             _text = "";
             _text = val.recognizedWords;
-            print('TEXTO A IMPRIMIR' + _text);
+            // print('TEXTO A IMPRIMIR' + _text);
             final splitted = _text.split(' ');
-<<<<<<< HEAD
-            print('PALABRA A SOLICITAR PARA REPRODUCCION' +
-                splitted[splitted.length - 1]);
-            url = 'http://127.0.0.1:5000/api?Query=' +
-                splitted[splitted.length - 1];
-            Data = await GetData(url);
-            var DecodedData = jsonDecode(Data);
-            print(DecodedData);
-            QueryText = DecodedData['Query'];
+            // print('PALABRA A SOLICITAR PARA REPRODUCCION' +
+            //     splitted[splitted.length - 1]);
+            // //url = 'http://127.0.0.1:5000/api?Query=' +
+            //     splitted[splitted.length - 1];
+            // //Data = await GetData(url);
+            // //var DecodedData = jsonDecode(Data);
+            // //print(DecodedData);
+            // //QueryText = DecodedData['Query'];
 
             //reproducirPalabra(splitted[splitted.length - 1]);
-            reproducirPalabra(QueryText);
-=======
+            reproducirPalabra(splitted[splitted.length - 1]);
             _text = splitted.last;
             print(_text); //text esta repitiendo
->>>>>>> 4ade504e25a464a486a2b7cd7736102151087ee7
           }),
         );
       }
